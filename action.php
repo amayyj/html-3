505ea3a62c60f370a3fbf2fffccd1fc00ea559ae
amayyj <346391770@qq.com>
2018-11-18 23:48:52 +0800

    Add files via upload

---
+++ action.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	
+    <meta http-equiv="Content" content="text/html;charse=utf-8"/>
+
+	<title>假装是php</title>
+</head>
+<body>
+
+ （假装此处）这是一个后台服务器文件
+
+
+</body>
+
+</html>
